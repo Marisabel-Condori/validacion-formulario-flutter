7360836aca2d17cabc6431156b1d837ae616675e
mari <mari.17ymy@gmail.com>
2020-06-03 01:47:26 -0400

    manteniendo el estado

--- lib/src/block/provider.dart
+++ lib/src/block/provider.dart
@@ -5,10 +5,19 @@ export 'package:validacion_de_formularios/src/block/login_block.dart';
 
 class Provider extends InheritedWidget {
 
+  static Provider _instancia;
+
+  factory Provider({Key key, Widget child}){
+    if(_instancia == null){
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+    return _instancia;
+  }
+
   final loginBloc  = LoginBloc();
 
-  Provider({Key key, Widget child})
-    :super(key: key, child: child);
+   Provider._internal({Key key, Widget child})
+     :super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
